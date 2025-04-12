acc2349c10aacaedf1b76574670aa31dbc67be76
Asker0707 <asker_original@mail.ru>
2025-04-12 17:59:12 +0300

    fix8

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import globals from "globals";
-export default defineConfig([
+export default [
   {
     ignores: ["**/dist/**", "**/node_modules/**", "**/coverage/**", "**/build/**", "**/__tests__/**", "**/jest.config.js**/"]
   },
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    ...eslint.configs.recommended,
     languageOptions: { globals: globals.browser }
   }
-]);
+];
